a0635b1e853ffe8171cccaa6f18158b6122275e2
Jan Rudolph <rudolph@biochem.mpg.de>
2018-06-05 13:04:00 +0200

    fix unit tests

--- PluginInterop/PluginInterop.Test/Resources/matrix_to_network.py
+++ PluginInterop/PluginInterop.Test/Resources/matrix_to_network.py
@@ -1,6 +1,6 @@
 import sys
 from perseuspy import pd, nx, write_networks
-_, paramfile, infile, outfolder = sys.argv
+_, infile, outfolder = sys.argv
 G = nx.random_graphs.barabasi_albert_graph(10, 3)
 networks_table, networks = nx.to_perseus([G])
 write_networks(outfolder, networks_table, networks)
--- PluginInterop/PluginInterop.Test/Resources/matrix_with_supp_tables.py
+++ PluginInterop/PluginInterop.Test/Resources/matrix_with_supp_tables.py
@@ -1,6 +1,6 @@
 import sys
 from perseuspy import pd
-_, paramfile, infile, outfile, suppfile1, suppfile2 = sys.argv
+_, infile, outfile, suppfile1, suppfile2 = sys.argv
 df = pd.read_perseus(infile)
 df.to_perseus(outfile)
 df.to_perseus(suppfile1)
