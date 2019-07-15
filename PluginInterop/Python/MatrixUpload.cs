bc0ceeb81f92ccf77b5c2d39fd54e8a7f941869f
Daniela Ferretti <ferretti.daniela@outlook.com>
2019-07-15 12:02:22 +0200

    Python Upload in Matrix fixed

--- PluginInterop/Python/MatrixUpload.cs
+++ PluginInterop/Python/MatrixUpload.cs
@@ -10,6 +10,11 @@ namespace PluginInterop.Python
         public override string Description => "Upload a matrix using Python";
         public override Bitmap2 DisplayImage => GraphUtils.ToBitmap2(Properties.Resources.python_logo_master_v3_TM_flattened);
 
+        protected override string CodeFilter => "Python script, *.py | *.py";
+
+        protected virtual string[] ReqiredPythonPackages => new[] { "perseuspy" };
+
+
         protected override FileParam ExecutableParam()
         {
             return Utils.CreateCheckedFileParam(InterpreterLabel, InterpreterFilter, TryFindExecutable, new []{"perseuspy"});
